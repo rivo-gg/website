cafdae06a69d2ad9de2be67270e83982f41fad46
Dominik K. <dominik@koch-bautechnik.de>
2024-02-05 20:42:38 +0000

    fix: add crawlable sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rivo.gg',
   integrations: [react(), tailwind(), sitemap()]
 });

d27ca225cfe579a31e47b8a7ea41c3130d08ecff
Dominik K <mezo.dsn@gmail.com>
2024-02-05 18:40:54 +0100

    fix: add compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), compress()]
 });

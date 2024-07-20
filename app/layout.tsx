9dd51681e63f3f79f794f76a7396d3bfaefb0d5b
Dominik Koch <dominik@koch-bautechnik.de>
2024-07-20 21:56:42 +0000

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import { ChakraProvider } from "@chakra-ui/react";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <ChakraProvider>
-        <body className={inter.className}>{children}</body>
-      </ChakraProvider>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }

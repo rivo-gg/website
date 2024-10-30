6094b6f25369b5a104517a7840b63738e414f4f5
Dominik Koch <dominik@koch-bautechnik.de>
2024-10-30 13:20:03 +0000

    fix: seo improvements

--- app/projects/cavescape/page.tsx
+++ app/projects/cavescape/page.tsx
@@ -74,10 +74,11 @@ export default function Cavescape() {
         json={`
         {
           "@context": "https://schema.org",
-          "@type": ["VideoGame", "MobileApplication", "GameApplication"],
+          "@type": "SoftwareApplication",
           "name": "Cavescape",
+          "description": "Cavescape is a co-op survival adventure game with one of the most fun mechanics around: Grappling Hooks! You play as Fred, a miner on a mission to make it back up the cave to be reunited with his best friend, Rocky the puppy, who was violently taken from him by Gorgath, the big ugly worm monster.",
           "operatingSystem": "ANDROID, IOS, WINDOWS",
-          "applicationCategory": "GameApplication",
+          "applicationCategory": "GameApplication"
         }
       `}
       />

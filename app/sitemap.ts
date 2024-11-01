90b5dac65b22e1815579f6d6827d798e59165f61
Dominik Koch <dominik@koch-bautechnik.de>
2024-11-01 10:39:05 +0100

    feat: add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,47 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://rivo.gg',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://rivo.gg/legal',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.1,
+      alternates: {
+        languages: {
+          de: 'https://rivo.gg/legal-de',
+        }
+      }
+    },
+    {
+      url: 'https://rivo.gg/privacy',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.1,
+    },
+    {
+      url: 'https://rivo.gg/tos',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.1,
+    },
+    {
+      url: 'https://rivo.gg/projects',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://rivo.gg/projects/cavescape',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ]
+}

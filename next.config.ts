e7f4cdee15778aeccec5d9adf9228324a73f46d2
Dominik Koch <dominik@koch-bautechnik.de>
2025-03-10 13:52:01 +0100

    fix: implement security headers

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,41 @@ const nextConfig: NextConfig = {
 	output: "standalone",
   reactStrictMode: true,
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  compiler: {
+		removeConsole: process.env.NODE_ENV === 'production' && {
+			exclude: ['error']
+		}
+    
+	},
+  async headers() {
+		return [
+			{
+				source: '/(.*)',
+				headers: [
+					{
+						key: 'Strict-Transport-Security',
+						value: 'max-age=31536000; includeSubDomains; preload'
+					},
+					{
+						key: 'Content-Security-Policy',
+						value: "frame-ancestors 'self'"
+					},
+					{
+						key: 'X-Frame-Options',
+						value: 'SAMEORIGIN'
+					},
+					{
+						key: 'Referrer-Policy',
+						value: 'origin-when-cross-origin'
+					},
+					{
+						key: 'X-Content-Type-Options',
+						value: 'nosniff'
+					}
+				]
+			}
+		]
+	},
 };
 
 const withMDX = createMDX();

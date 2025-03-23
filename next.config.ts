2fac06a62da94761ecb021b43542d6e70207c027
Dominik Koch <dominik@koch-bautechnik.de>
2025-03-23 20:43:04 +0100

    fix: add missing image remote pattern

--- next.config.ts
+++ next.config.ts
@@ -1,56 +1,59 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  output: "standalone",
   reactStrictMode: true,
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   compiler: {
-		removeConsole: process.env.NODE_ENV === 'production' && {
-			exclude: ['error']
-		}
-    
-	},
-   images: {
+    removeConsole: process.env.NODE_ENV === "production" && {
+      exclude: ["error"],
+    },
+  },
+  images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.marblecms.com',
+        protocol: "https",
+        hostname: "images.marblecms.com",
       },
       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+	  {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
       },
     ],
   },
   async headers() {
-		return [
-			{
-				source: '/(.*)',
-				headers: [
-					{
-						key: 'Strict-Transport-Security',
-						value: 'max-age=31536000; includeSubDomains; preload'
-					},
-					{
-						key: 'Content-Security-Policy',
-						value: "frame-ancestors 'self'"
-					},
-					{
-						key: 'X-Frame-Options',
-						value: 'SAMEORIGIN'
-					},
-					{
-						key: 'Referrer-Policy',
-						value: 'origin-when-cross-origin'
-					},
-					{
-						key: 'X-Content-Type-Options',
-						value: 'nosniff'
-					}
-				]
-			}
-		]
-	},
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains; preload",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self'",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;

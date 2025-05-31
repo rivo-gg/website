5a30f1318cae4982522b722216767d1819c92253
Dominik Koch <dominik@koch-bautechnik.de>
2025-05-31 11:19:08 +0200

    chore: update privacy policy page metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,60 +8,74 @@ import "@/styles/globals.css";
 import { SITE } from "@/lib/seo";
 
 const barlow = Barlow({
-	variable: "--font-sans",
-	subsets: ["latin"],
-	weight: ["400", "500", "600", "700"],
+  variable: "--font-sans",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
-	metadataBase: new URL("https://rivo.gg"),
-	alternates: {
-		canonical: "/",
-	},
-	title: SITE.title2,
-	description: SITE.description,
-	twitter: {
-		title: SITE.title2,
-		card: "summary_large_image",
-		description: SITE.description,
-	},
-	openGraph: {
-		title: SITE.title2,
-		description: SITE.description,
-	},
-	pinterest: {
-		richPin: true,
-	},
-	robots: {
-		index: true,
-		follow: true,
-	},
+  metadataBase: new URL("https://rivo.gg"),
+  alternates: {
+    canonical: "/",
+  },
+  title: SITE.title2,
+  description: SITE.description,
+  twitter: {
+    title: SITE.title2,
+    card: "summary_large_image",
+    description: SITE.description,
+  },
+  openGraph: {
+    title: SITE.title2,
+    description: SITE.description,
+  },
+  pinterest: {
+    richPin: true,
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export const viewport: Viewport = {
-	maximumScale: 5,
-	themeColor: "#3B6CF1",
+  maximumScale: 5,
+  themeColor: "#3B6CF1",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<>
-			<head>
-				<script defer data-domain="rivo.gg" src="https://stats.rivo.gg/js/script.js" />
-			</head>
-			<html lang="en">
-				<body className={cn("flex min-h-screen w-screen flex-col", barlow.className)}>
-					<ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-						<Navbar />
-						{children}
-						<Footer />
-					</ThemeProvider>
-				</body>
-			</html>
-		</>
-	);
+  return (
+    <>
+      <head>
+        <script
+          src="https://rybbit.rivo.gg/api/script.js"
+          data-site-id="2"
+          defer
+        />
+      </head>
+      <html lang="en">
+        <body
+          className={cn(
+            "flex min-h-screen w-screen flex-col",
+            barlow.className
+          )}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Navbar />
+            {children}
+            <Footer />
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
+  );
 }
